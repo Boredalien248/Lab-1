f0059944575908b60e26c38c54460f6e1a09d14c
Boredalien248 <kaustubh248@gmail.com>
2023-09-13 18:42:25 -0600

    Lab done now

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,3 +1,6 @@
 import requests
 
+stuff = requests.get("https://raw.githubusercontent.com/Boredalien248/Lab-1/main/lab1/lab1.py?token=GHSAT0AAAAAACHR3TOIGDUINVZKTGQCVS64ZICMRCA")
+print(stuff)
+
 print(requests.__version__)
