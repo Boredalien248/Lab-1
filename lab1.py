bc85ac2c7fff4ea3288bad5f0c31c3f51d7e543c
Kaustubh Mukherjee <kaustubh248@gmail.com>
2023-09-19 17:54:05 -0600

    missed something

--- lab1.py
+++ lab1.py
@@ -1,6 +1,9 @@
 import requests
 
-stuff = requests.get("https://raw.githubusercontent.com/Boredalien248/Lab-1/main/lab1/lab1.py?token=GHSAT0AAAAAACHR3TOIGDUINVZKTGQCVS64ZICMRCA")
-print(stuff)
+google = requests.get("https://www.google.com/")
+print(google)
+
+raw_file = requests.get("https://raw.githubusercontent.com/Boredalien248/Lab-1/main/lab1.py")
+print(raw_file)
 
 print(requests.__version__)
